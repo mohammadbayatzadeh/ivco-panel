0c109149c65e22c905d8601d50d944344776186f
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-10-11 14:20:57 +0330

    landing banner

--- src/components/elements/CoinIcon.js
+++ src/components/elements/CoinIcon.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import styles from "./CoinIcon.module.css";
+import Image from "next/image";
 
-function CoinIcon() {
+function CoinIcon({ title, x = 20, y = 40 , image }) {
   return (
-    <div>CoinIcon</div>
-  )
+    <div className={styles.container} style={{ top: y, right: x }}>
+      <span>{title}</span>
+      <Image
+        src={image}
+        width={50}
+        height={50}
+        alt="unreal engine"
+        className={styles.image}
+      />
+    </div>
+  );
 }
 
-export default CoinIcon
+export default CoinIcon;
--- src/components/modules/LandingBanner.js
+++ src/components/modules/LandingBanner.js
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./LandingBanner.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import CoinIcon from "../elements/CoinIcon";
 
 function LandingBanner() {
   return (
@@ -14,7 +15,7 @@ function LandingBanner() {
           <Link href="/login">SIGNUP</Link>
         </div>
       </div>
-     
+
       <Image
         src="images/Graphic-Purple-Wave.svg"
         width={800}
@@ -27,6 +28,11 @@ function LandingBanner() {
         height={600}
         className={styles.teal}
       />
+      <div className={styles.icons}>
+        <CoinIcon title="Trust Wallet" image='/images/tether.svg' x='20%' y={100} />
+        <CoinIcon title="Tether TRC20" image='/images/TWT.svg' x='30%' y={300} />
+        <CoinIcon title="Urneal Engine" image='/images/icons8-unreal-engine.svg' x='20%' y={450} />
+      </div>
     </section>
   );
 }
