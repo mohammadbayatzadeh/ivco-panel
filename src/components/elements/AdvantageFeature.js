65f434c14178397ae1ad3a153017a9afa4923a7a
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-10-12 16:12:42 +0330

    features completed

---
+++ src/components/elements/AdvantageFeature.js
@@ -0,0 +1,78 @@
+import React from "react";
+import styles from "./AdvantageFeature.module.css";
+import Image from "next/image";
+
+function AdvantageFeature() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.features}>
+        <Image
+          src="/images/Graphic-Layer-Top-p-1080.png"
+          width={700}
+          height={700}
+          alt="feature"
+          className={styles.playImage}
+        />
+        <Image
+          src="/images/Graphic-Layer-Coins-p-800.png"
+          width={700}
+          height={700}
+          alt="feature"
+          className={styles.playImage}
+        />
+        <Image
+          src="/images/Graphic-Layer-Bottom-p-800.png"
+          width={700}
+          height={700}
+          alt="feature"
+        />
+      </div>
+      <div className={styles.content}>
+        <h4>
+          Take full advantage of all that iwco has to offer today and in the
+          future
+        </h4>
+        <div className={styles.options}>
+          <div className={styles.option}>
+            <Image
+              src="/images/Icon-Reviews.svg"
+              width={100}
+              height={100}
+              alt="option"
+            />
+            <p>constant reviews and testing of our platform</p>
+          </div>
+          <div className={styles.option}>
+            <Image
+              src="/images/Icon-Secure.svg"
+              width={100}
+              height={100}
+              alt="option"
+            />
+            <p>security of your acconts</p>
+          </div>
+          <div className={styles.option}>
+            <Image
+              src="/images/Icon-Decentralized.svg"
+              width={100}
+              height={100}
+              alt="option"
+            />
+            <p>Online calculations and deposit of daily withrawals</p>
+          </div>
+          <div className={styles.option}>
+            <Image
+              src="/images/Icon-Experience.svg"
+              width={100}
+              height={100}
+              alt="option"
+            />
+            <p>Has the most powerful servers for gaming platforms</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdvantageFeature;
---
+++ src/components/elements/ProtocolFeature.js
@@ -0,0 +1,46 @@
+import React from "react";
+import styles from "./ProtocolFeature.module.css";
+import Image from "next/image";
+
+function ProtocolFeature() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <h4>Come for the memes, stay for the protocol.</h4>
+        <p>
+          While leverage trading on exotic pairs is great, having a Discord
+          server full of memes and alpha is even better.
+        </p>
+        <p>
+          Join our community of meme-loving masterminds and get the inside scoop
+          on what’s coming down the pipeline.
+        </p>
+      </div>
+
+      <div className={styles.features}>
+        <Image
+          src="/images/Graphic-Meme-Rocket-p-800.png"
+          width={700}
+          height={700}
+          alt="feature"
+          className={styles.playImage}
+        />
+        <Image
+          src="/images/Graphic-Meme-Cat-p-1080.png"
+          width={700}
+          height={700}
+          alt="feature"
+          className={styles.playImage}
+        />
+        <Image
+          src="/images/Graphic-Meme-Card-p-1080.png"
+          width={700}
+          height={700}
+          alt="feature"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default ProtocolFeature;
