28a48e63b9ace37d1a45be549257e3a91267bda3
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-10-21 13:44:55 +0330

    Update BuySection.js

--- src/components/modules/dashboard/BuySection.js
+++ src/components/modules/dashboard/BuySection.js
@@ -18,12 +18,13 @@ function BuySection() {
           <option value="option1">Buy Coin</option>
           <option value="option2">Convert Coin To Balance</option>
         </select>
-        <div className={styles.radio} onChange={e=>setValue(e.target.value)}>
+        <div className={styles.radio}>
           <input
             type="radio"
             id="customRadio1"
             name="customRadio"
             value="Direct"
+            onChange={(e) => setValue(e.target.value)}
           />
           <label htmlFor="customRadio1">Direct</label>
 
@@ -32,6 +33,7 @@ function BuySection() {
             id="customRadio2"
             name="customRadio"
             value="Balance"
+            onChange={(e) => setValue(e.target.value)}
           />
           <label htmlFor="customRadio2">Balance</label>
         </div>
@@ -39,10 +41,12 @@ function BuySection() {
           <label>Number:</label>
           <input name="number" type="text" />
         </div>
-        <div className={styles.input}>
-          <label>Trasnaction ID:</label>
-          <input name="number" type="text" />
-        </div>
+        {value === "Direct" ? (
+          <div className={styles.input}>
+            <label>Trasnaction ID:</label>
+            <input name="number" type="text" />
+          </div>
+        ) : null}
 
         <CopyToClipboard text={id} onCopy={() => Toast("id copied", "success")}>
           <div className={styles.id}>
