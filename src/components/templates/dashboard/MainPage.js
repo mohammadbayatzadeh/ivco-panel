e719520a9ea6c64a29b7b66f1b29f930a2bb046c
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-10-23 16:29:42 +0330

    main dashboard page chart added

--- src/components/templates/dashboard/MainPage.js
+++ src/components/templates/dashboard/MainPage.js
@@ -2,12 +2,19 @@ import React from "react";
 import styles from "./Mainpage.module.css";
 import BuySection from "@/components/modules/dashboard/BuySection";
 import InfoSection from "@/components/modules/dashboard/InfoSection";
+import LineChart from "@/components/elements/charts/LineChart";
 
 function MainPage() {
   return (
-    <div className={styles.container}>
-      <InfoSection />
-      <BuySection />
+    <div className={styles.body}>
+      <div className={styles.row}>
+        <InfoSection />
+        <BuySection />
+      </div>
+
+      <div className={styles.chart}>
+        <LineChart />
+      </div>
     </div>
   );
 }
