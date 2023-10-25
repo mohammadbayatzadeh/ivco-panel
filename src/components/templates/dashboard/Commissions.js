8845d3ddf541ae8a2ead08a9ff36abd4d3df48e3
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-10-25 16:49:29 +0330

    commissions page completed

--- src/components/templates/dashboard/Commissions.js
+++ src/components/templates/dashboard/Commissions.js
@@ -1,7 +1,11 @@
+"use client";
 //styles
+import { useState } from "react";
 import styles from "./Commissions.module.css";
 
 function Commissions() {
+  const [filter, setFilter] = useState("All");
+
   return (
     <div className={styles.container}>
       <h2>Commissions</h2>
@@ -10,9 +14,45 @@ function Commissions() {
         binary plan
       </p>
       <div className={styles.filter}>
-        <span>All</span>
-        <span>Pending</span>
-        <span>Paid</span>
+        <span
+          onClick={() => setFilter("All")}
+          name="All"
+          style={{ color: filter === "All" ? "gold" : "white" }}
+        >
+          All
+        </span>
+        <span
+          onClick={() => setFilter("Pending")}
+          name="Pending"
+          style={{ color: filter === "Pending" ? "gold" : "white" }}
+        >
+          Pending
+        </span>
+        <span
+          onClick={() => setFilter("Paid")}
+          name="Pending"
+          style={{ color: filter === "Paid" ? "gold" : "white" }}
+        >
+          Paid
+        </span>
+      </div>
+      <div className={styles.table}>
+        <div className={styles.headers}>
+          <span>C-ID</span>
+          <span>Day</span>
+          <span>Amount ($)</span>
+          <span>Status</span>
+          <span>Created at</span>
+          <span>operation</span>
+        </div>
+        <div className={styles.example}>
+          <span>id</span>
+          <span>7 july</span>
+          <span>20.00 $</span>
+          <span>Paid</span>
+          <span>5 july</span>
+          <span>example</span>
+        </div>
       </div>
     </div>
   );
