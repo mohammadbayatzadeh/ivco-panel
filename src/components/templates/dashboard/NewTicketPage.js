2905164c4ec39bd45514fdf776cd89c7069e07ec
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-10-31 13:39:05 +0330

    new ticket page

--- src/components/templates/dashboard/NewTicketPage.js
+++ src/components/templates/dashboard/NewTicketPage.js
@@ -1,7 +1,44 @@
-import React from "react";
+"use client";
+import { useState } from "react";
+import styles from "./NewTicketPage.module.css";
 
 function NewTicketPage() {
-  return <div>NewTicketPage</div>;
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+  });
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  const changeHandler = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  return (
+    <div className={styles.container}>
+      <h2>Tickets</h2>
+      <p>Create new ticket</p>
+      <form className={styles.form} onSubmit={submitHandler}>
+        <label>Title</label>
+        <input
+          type="text"
+          value={form.title}
+          onChange={changeHandler}
+          name="title"
+        />
+        <textarea
+          placeholder="Enter the text here"
+          value={form.description}
+          onChange={changeHandler}
+          name="description"
+        />
+        <button type="submit" className={styles.button}>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default NewTicketPage;
