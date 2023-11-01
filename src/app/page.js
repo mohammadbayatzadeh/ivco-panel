887346298fdc820f3ae16e5f4e742a89baddb292
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-11-01 16:09:51 +0330

    not found function configed

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,8 @@
+"use cleint";
 //templates
 import LandingPage from "@/components/templates/LandingPage";
 
 export default function Home() {
   return <LandingPage />;
 }
+export const dynamic = "force-catch";
