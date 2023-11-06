d5b0c82f0a26a48fde8df4d0ac26091553bc2883
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-11-06 14:59:57 +0330

    profile page design

--- src/components/elements/dashboard/InputBox.js
+++ src/components/elements/dashboard/InputBox.js
@@ -1,13 +1,21 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./InputBox.module.css";
 
 function InputBox({ title, label, value }) {
+  const [val, setVal] = useState(value);
   return (
     <div className={styles.container}>
       <p className={styles.title}>{title}</p>
       <div className={styles.input}>
         <label>{label}:</label>
-        <input name={label} type="text" />
+        <input
+          name={label}
+          type="text"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <button>submit</button>
       </div>
     </div>
   );
--- src/components/templates/dashboard/ProfilePage.js
+++ src/components/templates/dashboard/ProfilePage.js
@@ -1,15 +1,35 @@
 import React from "react";
 import styles from "./Pages.module.css";
 import InputBox from "@/components/elements/dashboard/InputBox";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+import { getNameFromEmail } from "@/utils/functions";
 
-function ProfilePage() {
+async function ProfilePage() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+  if (!session) redirect("/dashboard");
   return (
     <div className={styles.container}>
       <h2>Edit Profile</h2>
       <p>Edit your information according to each box</p>
-      <InputBox title="Change E-mail address" label='E-mail'/>
-      <InputBox title="Change Username" label='Username'/>
-      <InputBox title="Change WalletID" label='WalletId'/>
+      <InputBox
+        title="Change E-mail address"
+        label="E-mail"
+        value={session.user.email}
+      />
+      <InputBox
+        title="Change Username"
+        label="Username"
+        value={getNameFromEmail(session.user.email)}
+      />
+      <InputBox
+        title="Change WalletID"
+        label="WalletId"
+        value="lknvlsm45645ewofjisd"
+      />
     </div>
   );
 }
