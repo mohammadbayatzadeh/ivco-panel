26a9719622adadc9af23dbd10ba8a24b2e5a316a
mohammadbayatzadeh <mamadbayat777@gmail.com>
2023-11-22 16:05:34 +0330

    faq accordion

--- src/components/elements/Landing/items/FAQ.js
+++ src/components/elements/Landing/items/FAQ.js
@@ -1,18 +1,34 @@
-import React from "react";
+"use client";
+import React, { useRef, useState } from "react";
 
 //styles
 import styles from "./FAQ.module.css";
 
-function FAQ({ count }) {
-  let text = "";
+//icons
+import { GiTronArrow } from "react-icons/gi";
 
+function FAQ({ count }) {
+  const [show, setShow] = useState(false);
+  let text = "";
+  const contentEl = useRef();
   for (let i = 0; i < count; i++) {
     text += `Enter the registration section and enter your details registration`;
   }
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => setShow((prev) => !prev)}>
       <p className={styles.question}>Where do I start to register?</p>
-      <p className={styles.answer}>{text}</p>
+
+      <p
+        className={show ? ` ${styles.answer} ${styles.active}` : styles.answer}
+        style={
+          show
+            ? { height: contentEl.current.scrollHeight + 14 }
+            : { height: "0px" }
+        }
+        ref={contentEl}
+      >
+        {text}
+      </p>
     </div>
   );
 }
