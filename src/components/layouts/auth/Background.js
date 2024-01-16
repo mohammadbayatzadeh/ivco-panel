b1dde9f777e1cee8d5ec97ad0f2595e11114155a
mohammadbayatzadeh <mamadbayat777@gmail.com>
2024-01-16 15:50:29 +0330

    got dimentions

--- src/components/layouts/auth/Background.js
+++ src/components/layouts/auth/Background.js
@@ -1,7 +1,16 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+//styles
 import styles from "./Background.module.css";
 
 function Background({ children }) {
+  const [size, setSize] = useState(0);
+  useEffect(() => {
+    const w = window.innerWidth;
+    const h = window.innerHeight;
+    
+    console.log(w, h);
+  }, []);
   return (
     <div className={styles.body}>
       <div className={styles.squere}></div>
